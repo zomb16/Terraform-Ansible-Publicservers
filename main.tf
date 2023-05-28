64eff47f08a24ba7a0b975e6a5423a8ef9534a38
Zomb <awsrameshraju@gmail.com>
2023-05-29 01:10:56 +1000

    Added docker invfile and 6 webservers

--- main.tf
+++ main.tf
@@ -148,5 +148,53 @@ resource "aws_instance" "web-3" {
   }
 }
 
+resource "aws_instance" "web-4" {
+  ami                         = var.imagename
+  availability_zone           = "us-east-1a"
+  instance_type               = "t2.micro"
+  key_name                    = "devops"
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "TestServer04"
+    Env        = "Prod"
+    Owner      = "ramesh"
+    CostCenter = "ABCD"
+  }
+}
+
+resource "aws_instance" "web-5" {
+  ami                         = var.imagename
+  availability_zone           = "us-east-1a"
+  instance_type               = "t2.micro"
+  key_name                    = "devops"
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "TestServer05"
+    Env        = "Prod"
+    Owner      = "ramesh"
+    CostCenter = "ABCD"
+  }
+}
+
+resource "aws_instance" "web-6" {
+  ami                         = var.imagename
+  availability_zone           = "us-east-1a"
+  instance_type               = "t2.micro"
+  key_name                    = "devops"
+  subnet_id                   = aws_subnet.subnet1-public.id
+  vpc_security_group_ids      = ["${aws_security_group.allow_all.id}"]
+  associate_public_ip_address = true
+  tags = {
+    Name       = "TestServer06"
+    Env        = "Prod"
+    Owner      = "ramesh"
+    CostCenter = "ABCD"
+  }
+}
+
 
 
